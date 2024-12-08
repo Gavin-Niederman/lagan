a76cc40741a2f4bd155355df0ebd816d52886e3c
Gavin-Niederman <gavinniederman@gmail.com>
2024-12-07 17:29:20 -0800

    fix: append d only on debug builds

--- packages/ntcore-sys/build.rs
+++ packages/ntcore-sys/build.rs
@@ -3,6 +3,8 @@ use std::process::Command;
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let release = std::env::var("PROFILE").unwrap() == "release";
+    let maybe_d = if release { "" } else { "d" };
 
     cmake::Config::new(format!("{manifest_dir}/allwpilib"))
         .define("WITH_CSCORE", "OFF")
@@ -24,7 +26,7 @@ fn main() {
 
     println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rustc-link-search=native={out_dir}/lib");
-    println!("cargo:rustc-link-lib=ntcored");
-    println!("cargo:rustc-link-lib=wpiutild");
-    println!("cargo:rustc-link-lib=wpinetd");
+    println!("cargo:rustc-link-lib=ntcore{maybe_d}");
+    println!("cargo:rustc-link-lib=wpiutil{maybe_d}");
+    println!("cargo:rustc-link-lib=wpinet{maybe_d}");
 }
